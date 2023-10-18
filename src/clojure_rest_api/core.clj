9eef3ef82fb3d0004f65b8ab6c8831ac34985843
Artem Shubovych <shybovycha@gmail.com>
2023-10-18 15:55:19 +1100

    Add param validator

--- src/clojure_rest_api/core.clj
+++ src/clojure_rest_api/core.clj
@@ -7,8 +7,15 @@
   :available-media-types ["text/html"]
   :handle-ok (fn [_] "<html><body><h1>Hello, Internet.</h1></body></html>"))
 
+(defresource validator [n]
+  :available-media-types ["text/html"]
+  :malformed? (fn [_] (odd? n))
+  :handle-ok (fn [_] (format "<html><body><h1>%d is an good even number</h1></body></html>" n))
+  :handle-malformed (fn [_] (format "%d is a bad number" n)))
+
 (defroutes app
-  (GET "/" [] (root)))
+  (GET "/" [] (root))
+  (GET "/validate/:n" [n] (validator (Integer/parseInt n))))
 
 (def handler
   (-> app
